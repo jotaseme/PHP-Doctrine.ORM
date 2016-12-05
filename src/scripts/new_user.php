89b23f613912f1b2706ba19d9f8ce685c3e5928d
Jose Maria <jotaememoreno@gmail.com>
2016-12-05 20:18:40 +0100

    Cleaning code

--- src/scripts/new_user.php
+++ src/scripts/new_user.php
@@ -2,15 +2,12 @@
 
 require_once __DIR__ . '/../../bootstrap.php';
 
-
 if ($argc < 2) {
     echo "$argv[0] <username> <email> <password>" . PHP_EOL;
     exit();
 }
 
-
 /** @var \MiW16\Results\Entity\User $user */
-
 $user = new \MiW16\Results\Entity\User();
 $user->setUsername($argv[1]);
 $user->setEmail($argv[2]);
