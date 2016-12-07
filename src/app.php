16a59966b007b2f4294cc6b7e4541240abab2efa
Jose Maria <jotaememoreno@gmail.com>
2016-12-07 22:33:55 +0100

    Added route for getting the edit form

--- src/app.php
+++ src/app.php
@@ -4,5 +4,5 @@ use Symfony\Component\Routing;
 $routes = new Routing\RouteCollection();
 $routes->add('users', new Routing\Route('/users'));
 $routes->add('show_one_user', new Routing\Route('/users/{id}'));
-
+$routes->add('edit_one_user', new Routing\Route('/users/{id}/edit'));
 return $routes;
