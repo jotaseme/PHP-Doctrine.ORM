d2a446972c90cf0246de4c71378ce0b9d2691177
Jose Maria <jotaememoreno@gmail.com>
2016-12-10 12:22:58 +0100

    Added root variable for storing the web server root url

--- src/Controllers/show_one_user.php
+++ src/Controllers/show_one_user.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../../bootstrap.php';
+$root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
 
 echo "<h1>Detalle de usuario</h1>";
 
@@ -11,7 +12,7 @@ $user = $userRepository->find($id);
 if($user){
     echo  "<h3>" . $user->getId(). " " . $user->getUsername() ."</h3>" . PHP_EOL ;
     echo $user->getEmail();
-    echo ' <a href="http://localhost:8888/users/'.$user->getId().'/edit"><button>Edit user</button></a> ' . ' <button>Delete user</button>';
+    echo ' <a href="'.$root . 'users/'.$user->getId().'/edit"><button>Edit user</button></a> ' . ' <button>Delete user</button>';
 }else{
     echo "No existe usuario con la id: " . $id;
 }
